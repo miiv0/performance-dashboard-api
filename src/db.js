f0681317539ef534dcc3b3971cbada2cf77d1d69
miiv0 <minzana2002@gmail.com>
2026-02-24 20:10:33 -0500

    add connection status

---
+++ src/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.on("connect", () => {
+    console.log("PostgreSQL connected");
+});
+
+pool.on("error", (err) => {
+    console.error("Unexpected error", err);
+    process.exit(1);
+});
+
+module.exports = pool;
